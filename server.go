d6a293072c445935ad793d0ca606501a19d529c8
wenchajun <995729579@qq.com>
2021-09-11 15:53:05 +0800

    发送消息
    Signed-off-by: wenchajun <995729579@qq.com>

--- server.go
+++ server.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
 
 type Server struct {
-	Ip string
+	Ip   string
 	Port int
 	//在线用户列表
 	OnlineMap map[string]*User
-	maplock sync.RWMutex
+	maplock   sync.RWMutex
 	//消息广播的channel
 	Message chan string
-
 }
+
 //创建一个server接口
-func Newserver(ip string,port int) *Server {
-	server:=&Server{
+func Newserver(ip string, port int) *Server {
+	server := &Server{
 		Ip:        ip,
 		Port:      port,
 		OnlineMap: make(map[string]*User),
@@ -29,13 +30,13 @@ func Newserver(ip string,port int) *Server {
 }
 
 //监听message广播消息channe的goroutine，一旦有消息就发给全部的在线user
-func (server Server) ListenMessage()  {
-	for  {
-		msg:=<-server.Message
+func (server Server) ListenMessage() {
+	for {
+		msg := <-server.Message
 		//发送给全部user
 		server.maplock.Lock()
-		for _, cli :=range server.OnlineMap{
-			cli.C<- msg
+		for _, cli := range server.OnlineMap {
+			cli.C <- msg
 		}
 		server.maplock.Unlock()
 
@@ -44,56 +45,64 @@ func (server Server) ListenMessage()  {
 }
 
 //广播消息
-func (server *Server) BroadCast(user *User,msg string)  {
-	sendMsg:= "["+user.Addr+"]"+user.Name+":"+msg
-	server.Message<-sendMsg
+func (server *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	server.Message <- sendMsg
 
 }
 
-func(server *Server) Handler (coon net.Conn){
+func (server *Server) Handler(conn net.Conn) {
 
-fmt.Println("链接建立成功")
-user:=NewUser(coon)
-//用户上限，将用户加入到onlinemap中
-server.maplock.Lock()
-server.OnlineMap[user.Name]=user
-server.maplock.Unlock()
-//广播当前用户上线消息
-server.BroadCast(user,"已上线")
-//当前handler阻塞
-select {
+	fmt.Println("链接建立成功")
+	user := NewUser(conn, server)
+
+	user.Online()
+	//接收客户端发送的信息
+	go func() {
+		buff := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buff)
+			if n == 0 {
+				user.Offline()
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("coon read err:", err)
+				return
+			}
+			//提取用户的消息 （去除“\n”）
+			msg := string(buff[:n-1])
+			//用户针对msg进行处理
+			user.Domessage(msg)
+
+		}
+	}()
+
+	//当前handler阻塞
+	select {}
 
 }
 
-
-}
-
-
-
-func (server *Server) Start()  {
+func (server *Server) Start() {
 	//socker listen
-	listener,err:=net.Listen("tcp",fmt.Sprintf("%s:%d",server.Ip,server.Port))
-	if err !=nil{
-		fmt.Println("net.listen err:",err)
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", server.Ip, server.Port))
+	if err != nil {
+		fmt.Println("net.listen err:", err)
 	}
 	//close lisener
 	defer listener.Close()
-  go server.ListenMessage()
-	for{
+	go server.ListenMessage()
+	for {
 		//accept
 
-      conn,err:=listener.Accept()
-		if err !=nil{
-			fmt.Println("listener eaccept:",err)
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener eaccept:", err)
 			continue
 		}
 
 		//do handler
-        go server.Handler(conn)
+		go server.Handler(conn)
 	}
-
-
-
-
 
 }
--- user.go
+++ user.go
@@ -3,37 +3,56 @@ package main
 import "net"
 
 type User struct {
-	Name string
-	Addr string
-	C  chan string
-	Conn net.Conn
+	Name   string
+	Addr   string
+	C      chan string
+	Conn   net.Conn
+	Server *Server
 }
+
 //创建一个用户的api
-func NewUser(coon net.Conn) *User{
-	userAddr :=coon.RemoteAddr().String()
-	user:=&User{
-		Name: userAddr,
-		Addr: userAddr,
-		C:    make(chan string),
-		Conn: coon,
+func NewUser(coon net.Conn, server *Server) *User {
+	userAddr := coon.RemoteAddr().String()
+	user := &User{
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		Conn:   coon,
+		Server: server,
 	}
-		//启动监听当前user channel消息的goroutine
+	//启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
 	return user
 
 }
 
-//监听当前user channe的方法，一旦有消息，就直接发送给对端客户端
-func (user *User) ListenMessage()  {
+func (user *User) Online() {
+	//用户上线，将用户加入到onlinemap中
+	user.Server.maplock.Lock()
+	user.Server.OnlineMap[user.Name] = user
+	user.Server.maplock.Unlock()
+	//广播当前用户上线消息
+	user.Server.BroadCast(user, "已上线")
+}
+func (user *User) Offline() {
+	//用户下线，将用户剔除onlinemap中
+	user.Server.maplock.Lock()
+	delete(user.Server.OnlineMap, user.Name)
+	user.Server.maplock.Unlock()
+	//广播当前用户下线消息
+	user.Server.BroadCast(user, "当前用户已下线")
+}
+func (user *User) Domessage(msg string) {
+	user.Server.BroadCast(user, msg)
+}
 
-	for{
-		msg:=<-user.C
-		user.Conn.Write([]byte (msg+"\n"))
+//监听当前user channe的方法，一旦有消息，就直接发送给对端客户端
+func (user *User) ListenMessage() {
+
+	for {
+		msg := <-user.C
+		user.Conn.Write([]byte (msg + "\n"))
 
 	}
 
-
-
-
 }
-
